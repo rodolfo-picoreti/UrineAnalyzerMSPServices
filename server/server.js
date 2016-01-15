0e4f692d4de3927621952aae801c0bf831539b66
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2016-01-15 12:27:55 +0100

    new usage example

--- server/server.js
+++ server/server.js
@@ -2,12 +2,22 @@ function logger(status, results) {
 	console.log(`${status}: ${results}`)
 }
 
+setInterval(function() {
+	if (Services.isConnected()) {
+		Services.call('configureLocks', [ 
+			{ id: 2, value: 1 } // lock heaters
+		], logger)		
+	}
+}, 1000)
+
+
 Meteor.startup(function () {
 
 	Services.initialize(function() {
 
 		Services.on('storeSample', function (args) {
-			console.log(`   Heater: ${args.heater.raw}   ${args.heater.temperature}\nPreheater: ${args.preheater.raw}   ${args.preheater.temperature}`)
+			console.log(`   Heater: ${args.heater.raw}   ${args.heater.temperature}`)
+			console.log(`Preheater: ${args.preheater.raw}   ${args.preheater.temperature}`)
 		})
 
 		Services.call('startSampling', { samplingTime: 1000 }, function(status) {
@@ -20,7 +30,7 @@ Meteor.startup(function () {
 				{ id: 2, value: 0 } // Unlock heaters
 			], logger)		
 		}, 1000)
-		
+
 		Services.call('configureHeaters', [ 
 			{ id: 2, value: 100 }, 
 			{ id: 3, value: 110 } 
